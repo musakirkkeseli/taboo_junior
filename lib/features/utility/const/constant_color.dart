a3c60c4fb7d7e91a77b1a25c75edefddec5ead5e
musakirkkeseli <musakirkkeseli@gmail.com>
2025-11-18 16:31:41 +0300

    add new colors. to ConstColor

--- lib/features/utility/const/constant_color.dart
+++ lib/features/utility/const/constant_color.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ConstColor {
+  static const buttonTextColor = Color(0xFF5C9E00);
+  static const textColor = Color(0xFF7990C7);
+  static const tabuWordColor = Color(0xFF005CC6);
+  static const customTextColor = Color(0xFFCE7C00);
+  static const otherTextColor = Color(0xFFFFE100);
+  static const scoreTextColor = Color(0xFFFFE13D);
+  static const white = Colors.white;
+  static const gameLeftColor = Color(0xFFC600A5);
+  static const transparent = Color(0x00000000);
+  static const backbg = Color(0xff0069BF);
+  static const buttonBorder = Color(0xff25C1FF);
+
+
+
   static const primaryColor = Color(0xffA31D1D);
   static const secondColor = Color(0xffE5D0AC);
-  static const white = Colors.white;
+  static const blue5E7DC5 = Color(0xFF5E7DC5);
+  static const green6C9831 = Color(0xFF6C9831);
 }
