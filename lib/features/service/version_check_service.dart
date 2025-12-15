d1ef8835448ff33128328de4dd2cdac4b0365406
musakirkkeseli <musakirkkeseli@gmail.com>
2025-12-15 13:59:55 +0300

    created VersionCheckService

---
+++ lib/features/service/version_check_service.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:package_info_plus/package_info_plus.dart';
+
+import '../model/version_check_request_model.dart';
+import '../model/version_check_response_model.dart';
+
+class VersionCheckService {
+  static const String _baseUrl = 'https://api.tabumium.com';
+  static const String _checkUpdateEndpoint = '/check-update';
+
+  /// Checks if app version is up to date
+  /// Returns VersionCheckResponseModel or throws exception on error
+  Future<VersionCheckResponseModel> checkVersion() async {
+    try {
+      // Get package info
+      final packageInfo = await PackageInfo.fromPlatform();
+      final version = packageInfo.version;
+      final buildNumber = packageInfo.buildNumber;
+
+      // Detect platform
+      String deviceType;
+      if (Platform.isIOS) {
+        deviceType = 'ios';
+      } else if (Platform.isAndroid) {
+        deviceType = 'android';
+      } else {
+        deviceType = 'unknown';
+      }
+
+      // Create request body
+      final request = VersionCheckRequestModel(
+        version: version,
+        buildNo: buildNumber,
+        deviceType: deviceType,
+      );
+      print('Version check request: ${request.toJson()}');
+
+      // Make POST request
+      final url = Uri.parse('$_baseUrl$_checkUpdateEndpoint');
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(request.toJson()),
+      );
+      print('Version check response: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+        return VersionCheckResponseModel.fromJson(jsonResponse);
+      } else {
+        throw Exception('Failed to check version: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Version check error: $e');
+    }
+  }
+}
