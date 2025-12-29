3109ae4e9e4201d5ededc57c86838bd5312072eb
musakirkkeseli <musakirkkeseli@gmail.com>
2025-12-29 17:01:07 +0300

    add token, admobBannerAndroid, admobBannerIos, admobInterstitialAndroid ve  admobInterstitialIos to env and use with Environment class

--- lib/features/utility/const/environment.dart
+++ lib/features/utility/const/environment.dart
@@ -8,4 +8,29 @@ class Environment {
   static String get backendUrl {
     return dotenv.env["BACKEND_URL"] ?? "";
   }
+
+  static String get token {
+    return dotenv.env["TOKEN"] ?? "";
+  }
+
+  // AdMob Ad Unit IDs
+  static String get admobBannerAndroid {
+    return dotenv.env["ADMOB_BANNER_ANDROID"] ?? "";
+    // "'ca-app-pub-3940256099942544/6300978111'"; // Test ID
+  }
+
+  static String get admobBannerIos {
+    return dotenv.env["ADMOB_BANNER_IOS"] ?? "";
+    // "ca-app-pub-3940256099942544/2934735716"; // Test ID
+  }
+
+  static String get admobInterstitialAndroid {
+    return dotenv.env["ADMOB_INTERSTITIAL_ANDROID"] ?? "";
+    // "ca-app-pub-3940256099942544/1033173712"; // Test ID
+  }
+
+  static String get admobInterstitialIos {
+    return dotenv.env["ADMOB_INTERSTITIAL_IOS"] ?? "";
+    // "ca-app-pub-3940256099942544/4411468910"; // Test ID
+  }
 }
