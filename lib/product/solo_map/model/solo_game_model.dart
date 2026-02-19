1f382441a524c845647ca9be5f8d1ea31eacd28c
musakirkkeseli <musakirkkeseli@gmail.com>
2026-02-19 11:35:17 +0300

    update SoloGame model

--- lib/product/solo_map/model/solo_game_model.dart
+++ lib/product/solo_map/model/solo_game_model.dart
@@ -23,22 +23,48 @@ class SoloGameModel {
 
 class SoloGame {
   int? id;
-  String? word;
-  List<String>? clues;
+  List<SoloGameWord>? words;
 
-  SoloGame({this.id, this.word, this.clues});
+  SoloGame({this.id, this.words});
 
   SoloGame.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    word = json['word'];
-    clues = json['clues'].cast<String>();
+    if (json['words'] != null) {
+      words = <SoloGameWord>[];
+      json['words'].forEach((v) {
+        words!.add(SoloGameWord.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
+    if (words != null) {
+      data['words'] = words!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class SoloGameWord {
+  String? word;
+  List<String>? clues;
+  String? jokerClue;
+
+  SoloGameWord({this.word, this.clues, this.jokerClue});
+
+  SoloGameWord.fromJson(Map<String, dynamic> json) {
+    word = json['word'];
+    clues = json['clues'].cast<String>();
+    jokerClue = json['jokerClue'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
     data['word'] = word;
     data['clues'] = clues;
+    data['jokerClue'] = jokerClue;
     return data;
   }
 }
